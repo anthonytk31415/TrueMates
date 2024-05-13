03a86f97dfa3d4aeb01be5c7f99f3acf97a7b43e
anthonytk31415 <anthonytk31415@gmail.com>
2024-05-13 13:53:09 -0700

    added dotenv and config files

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
 console.log("hello world");
+
+require('dotenv').config();
+const Sequelize = require('sequelize');
+
+const sequelize = new Sequelize(
+        'truemates', 
+        process.env.DB_USERNAME, 
+        process.env.DB_KEY, {
+    host: process.env.DB_HOST,
+    dialect: 'postgres',
+});
+
+sequelize
+    .authenticate()
+    .then(() => {
+        console.log("db connection successful. ");
+    })
+    .catch((err) => {
+        console.error("unable to connect to db.", err);
+    });
+
